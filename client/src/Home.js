25e362c9f68f36c8c1d3b650eb5f5c581d71b658
illuminateor <o.mudrak93@gmail.com>
2022-01-13 16:06:07 +0100

    Add tailwind, react-router and axios

---
+++ client/src/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex justify-center content-center container">
+      <h1 className="text-lg">Todo List</h1>
+    </div>
+  );
+}
+
+export default Home;
